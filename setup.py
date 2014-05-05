c5c3d1c14382c37a6a34b733c151cc840bc5b892
Ethan Furman <ethan@stoneleaf.us>
2014-05-05 10:37:18 -0700

    require 'ordereddict' for Python versions 2.6 and below

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-import sys, os
+import os
+import sys
 from distutils.core import setup
+
+if sys.version_info[:2] < (2, 7):
+    required = ['ordereddict']
+else:
+    required = []
 
 long_desc = open('enum/doc/enum.rst').read()
 
 setup( name='enum34',
-       version='0.9.24',
+       version='1.0',
        url='https://pypi.python.org/pypi/enum34',
        packages=['enum'],
        package_data={
@@ -20,6 +26,7 @@ setup( name='enum34',
        description='Python 3.4 Enum backported to 3.3, 3.2, 3.1, 2.7, 2.6, 2.5, and 2.4',
        long_description=long_desc,
        provides=['enum'],
+       install_requires=required,
        author='Ethan Furman',
        author_email='ethan@stoneleaf.us',
        classifiers=[
@@ -27,6 +34,11 @@ setup( name='enum34',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
-            'Topic :: Software Development' ],
-     )
+            'Topic :: Software Development',
+            'Programming Language :: Python :: 2.4',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            ]
 
