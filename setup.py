13ae2859610c6dfb05761ab0bbd7f9a6f337df79
Ethan Furman <ethan@stoneleaf.us>
2016-05-04 16:54:46 -0700

    fix description and README

--- setup.py
+++ setup.py
@@ -2,23 +2,30 @@ import setuptools
 from distutils.core import setup
 
 long_desc = '''\
-`aenum` includes the new Python stdlib enum module available in Python 3.4
-backported for previous versions of Python from 2.7 and 3.3+
-tested on 2.7, and 3.3+
+Advanced Enumerations (compatible with Python's stdlib Enum), NamedTuples, and NamedConstants
 
+aenum includes a Python stdlib Enum-compatible data type, as well as a metaclass-based NamedTuple implementation and a NamedConstant class.
 
-An `Enum` is a set of symbolic names (members) bound to unique, constant
-values.  Within an enumeration, the members can be compared by identity, and
-the enumeration itself can be iterated over.
+An Enum is a set of symbolic names (members) bound to unique, constant values. Within an enumeration, the members can be compared by identity, and the enumeration itself can be iterated over.  If using Python 3 there is built-in support for unique values, multiple values, auto-numbering, and suspension of aliasing (members with the same value are not identical), plus the ability to have values automatically bound to attributes.
 
-A `NamedTuple` is a class-based, fixed-length tuple with a name for each
-possible position accessible using attribute-access notation.
+A NamedTuple is a class-based, fixed-length tuple with a name for each possible position accessible using attribute-access notation as well as the standard index notation.
 
-A `NamedConstant` is a class whose members cannot be rebound;  it lacks all other
-`Enum` capabilities, however; consequently, it can have duplicate values.
-There is also a `module` function that can insert the `NamedConstant` class
-into `sys.modules` where it will appear to be a module whose top-level
-names cannot be rebound.
+A NamedConstant is a class whose members cannot be rebound; it lacks all other Enum capabilities, however; consequently, it can have duplicate values.
+
+Utility functions include:
+
+- skip: class that prevents attributes from being converted to a
+        constant or enum member
+
+- module: inserts NamedConstant and Enum classes into sys.modules
+          where it will appear to be a module whose top-level names
+          cannot be rebound
+
+- extend_enum: add new members to enumerations after creation
+
+- enum: helper class for creating members with keywords
+
+- constant: helper class for creating constant members
 '''
 
 data = dict(
