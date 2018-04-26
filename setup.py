96e8b85e2842cbc22d6933042853866d194aa9d3
Ethan Furman <ethan@stoneleaf.us>
2018-04-25 18:09:24 -0700

    version 2.1.2

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ Utility functions include:
 
 data = dict(
        name='aenum',
-       version='2.1.2a1',
+       version='2.1.2',
        url='https://bitbucket.org/stoneleaf/aenum',
        packages=['aenum'],
        package_data={
