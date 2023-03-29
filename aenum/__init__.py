a048a1a827417394b8e81eec589c9ac2ea109c0f
Ethan Furman <ethan@stoneleaf.us>
2023-03-29 10:12:43 -0700

    version 3.1.12

--- aenum/__init__.py
+++ aenum/__init__.py
@@ -63,7 +63,7 @@ __all__ = [
 if sqlite3 is None:
     __all__.remove('SqliteEnum')
 
-version = 3, 1, 12, 1
+version = 3, 1, 12
 
 # shims
 try:
--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ Utility functions include:
 
 data = dict(
        name='aenum',
-       version='3.1.12a1',
+       version='3.1.12',
        url='https://github.com/ethanfurman/aenum',
        packages=['aenum'],
        package_data={
