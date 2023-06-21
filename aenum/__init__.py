021762580206c7ba816d3f795617383027d87a4c
Ethan Furman <ethan@stoneleaf.us>
2023-06-21 16:48:04 -0700

    bump version

--- aenum/__init__.py
+++ aenum/__init__.py
@@ -1,7 +1,7 @@
 """Python Advanced Enumerations & NameTuples"""
 from __future__ import print_function
 
-version = 3, 1, 13
+version = 3, 1, 14, 1
 
 # imports
 from ._common import *
--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ Utility functions include:
 
 data = dict(
        name='aenum',
-       version='3.1.13',
+       version='3.1.14a1',
        url='https://github.com/ethanfurman/aenum',
        packages=['aenum'],
        package_data={
