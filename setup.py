7a0d4f5a29a3dceec5f8f96551957cc717d9f5db
Ethan Furman <ethan@stoneleaf.us>
2023-06-21 21:13:24 -0700

    version 3.1.14

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ Utility functions include:
 
 data = dict(
        name='aenum',
-       version='3.1.14a1',
+       version='3.1.14',
        url='https://github.com/ethanfurman/aenum',
        packages=['aenum'],
        package_data={
